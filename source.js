2c4f4a188fe69138a3bc68436eedcc4b671116bf
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-04-09 10:39:04 -0400

    added overlay effect that responds to two different click events

--- source.js
+++ source.js
@@ -54,12 +54,20 @@ const myLibrary = (() => {
             const authorp = document.createElement('p');
             const pagesp = document.createElement('p');
             const readp = document.createElement('p');
+            const extra = document.createElement('p');                          //for the extra content
+            const titleWrapper = document.createElement('div');
+            const extraWrapper = document.createElement('div');
     
+            //Adds a class to extra
+            extraWrapper.classList.add('extraWrapper');
+            titleWrapper.classList.add('titleWrapper');
+
             //Adds the text content
             titleh3.textContent = obj.title;
-            authorp.textContent = "By: " + obj.author;
+            authorp.textContent = "Author: " + obj.author;
             pagesp.textContent = "Number of pages: " + obj.pages;
             readp.textContent = "This book is: " + obj.read;
+            extra.textContent = "Additional Information: Here is a ton of extra information, such as longer name, information on the text, whether we liked the book, etc.";   //Extra content
     
             //Creates and appends an update and delete button to the newBook div
             const delButton = document.createElement('button');
@@ -67,20 +75,33 @@ const myLibrary = (() => {
             delButton.setAttribute('id', index);
             delButton.textContent = 'X';
 
-            const updateButton = document.createElement('button');
-            updateButton.classList.add('updateButton');
-            updateButton.setAttribute('id', index);
-            updateButton.textContent = 'Change read status';
+            // const updateButton = document.createElement('button');
+            // updateButton.classList.add('updateButton');
+            // updateButton.setAttribute('id', index);
+            // updateButton.textContent = 'Update read status';
 
+            
+            //Creates the additional information button
+            const additionalInfoButton = document.createElement('button');
+            additionalInfoButton.classList.add('infoButton');
+            additionalInfoButton.textContent = 'See Additional Information or Update';
+            additionalInfoButton.setAttribute('id', index);
+            
             index++;
-    
+
+
+
             //Append the new fields
             newBook.appendChild(delButton);
-            newBook.appendChild(titleh3);
+            titleWrapper.appendChild(titleh3);
+            newBook.appendChild(titleWrapper);
             newBook.appendChild(authorp);
             newBook.appendChild(pagesp);
             newBook.appendChild(readp);
-            newBook.appendChild(updateButton);
+            // extraWrapper.appendChild(extra);
+            // newBook.appendChild(extraWrapper);                                         //Extra content
+            // newBook.appendChild(updateButton);
+            newBook.appendChild(additionalInfoButton);
             bookWrapper.appendChild(newBook);
 
 
@@ -97,6 +118,10 @@ const myLibrary = (() => {
 
         //displays the data to the page
         displayData();
+    };
+
+    const viewBook = (index) => {
+
     };
 
 
@@ -257,20 +282,42 @@ submitBookButton.addEventListener('click', function(e) {
 
 
 // For the Overlay
+
 let overlayOn = false;
 
 const overlay = (() => {
+
     const on = () => {
-        document.querySelector(".overlay").style.display = "flex";
+        document.querySelector('.overlay').style.display = "flex";
         overlayOn = true;
     }
 
     const off = () => {
-        document.querySelector(".overlay").style.display = "none";
+        document.querySelector('.overlay').style.display = "none";
         overlayOn = false;
     }
 
-    return {on, off};
+    const addBookOn = () => {
+        document.querySelector('#formHolder').style.display = "flex";
+        overlayOn = true;
+    }
+
+    const addBookOff = () => {
+        document.querySelector('#formHolder').style.display = "none";
+        overlayOn = false;
+    }
+
+    const editBookOn = () => {
+        document.querySelector('.bookOverlayWrapper').style.display = "flex";
+        overlayOn = true;
+    }
+
+    const editBookOff = () => {
+        document.querySelector('.bookOverlayWrapper').style.display = "none";
+        overlayOn = false;
+    }
+
+    return {on, off, addBookOn, addBookOff, editBookOn, editBookOff};
 })();
 
 // Query selector to turn the overlay on
@@ -278,7 +325,7 @@ const overlayButton = document.querySelector('#newBookButton');
 overlayButton.addEventListener('click', () => {
     if (!overlayOn){
         overlay.on();
-        overlayOn = true;
+        overlay.addBookOn();
     }
 });
 
@@ -287,17 +334,44 @@ const closeButton = document.querySelector('#closeOverlay');
 closeButton.addEventListener('click', () => {
     if (overlayOn){
         overlay.off();
-        overlayOn = false;
+        overlay.addBookOff();
     }
 
 });
 
-
-// event handles for the delete book buttons and update read status buttons
-
+// Important function for assigning an event handler to each button created by the DOM
 function hasClass(elem, className) {
     return elem.classList.contains(className);
 }
+
+// event handlers for the additional information buttons. This will bring up an overlay with more detailed information on the book
+const additionalInfoButtons = document.querySelectorAll('.infoButton');
+bookWrapper.addEventListener('click', function(e) {
+    if (hasClass(e.target, 'infoButton')) {
+        let index = e.target.id;        //Grabs the index so that we can pull up the correct book - implement later
+        // turns on the overlay
+        if (!overlayOn){
+            overlay.on();
+            overlay.editBookOn();
+        }
+    }
+});
+
+const closeEditOverlay = document.querySelector('#closeEditOverlay');
+closeEditOverlay.addEventListener('click', () => {
+    if (overlayOn){
+        overlay.off();
+        overlay.editBookOff();
+    }
+});
+
+
+
+
+
+
+// event handlers for the delete book buttons and update read status buttons
+
 
 
 const removeButtons = document.querySelectorAll('.delButton');
@@ -306,12 +380,13 @@ bookWrapper.addEventListener('click', function(e) {
         let index = e.target.id;
         myLibrary.deleteBook(index);
     }
-})
+});
 
-const updateButtons = document.querySelectorAll('.updateButton');
-bookWrapper.addEventListener('click', function(e) {
-    if (hasClass(e.target, 'updateButton')) {
-        let index = e.target.id;
-        myLibrary.updateBook(index);
-    }
-})
+// Redacted due to decision to create a book overlay
+// const updateButtons = document.querySelectorAll('.updateButton');
+// bookWrapper.addEventListener('click', function(e) {
+//     if (hasClass(e.target, 'updateButton')) {
+//         let index = e.target.id;
+//         myLibrary.updateBook(index);
+//     }
+// })
